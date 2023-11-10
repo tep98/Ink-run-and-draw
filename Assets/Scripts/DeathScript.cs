1805f4cd9f5c5f996a110b4017b61d6629662d4a
Zaklzzzy <zaletin123@gmail.com>
2023-11-10 12:37:28 +0300

    fix: fix bonus level

--- Assets/Scripts/DeathScript.cs
+++ Assets/Scripts/DeathScript.cs
@@ -7,19 +7,9 @@ public class DeathScript : MonoBehaviour
 {
     [SerializeField] private Animator screenAnim;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        KillPlayer();
-        Destroy(other.gameObject);
-    }
-
     public void KillPlayer()
     {
         screenAnim.SetBool("killPlayer", true);
     }
 
-    public void RestartScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
 }
